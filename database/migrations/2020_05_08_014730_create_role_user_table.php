2091dd1b905e262e1dbf80dd5822091ed86e03b7
Diego <diego.neumann.drese@gmail.com>
2022-06-16 17:33:06 -0300

    Update 2020_05_08_014730_create_role_user_table.php
    Adjust name table

--- database/migrations/2020_05_08_014730_create_role_user_table.php
+++ database/migrations/2020_05_08_014730_create_role_user_table.php
@@ -13,7 +13,7 @@ return new class extends Migration {
     public function up() {
         Schema::create('role_user', function (Blueprint $table) {
             $table->bigInteger('role_id')->unsigned()->index();
-            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+            $table->foreign('role_id')->references('id')->on('role')->onDelete('cascade');
             $table->bigInteger('user_id')->unsigned()->index();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->primary(['role_id', 'user_id']);
