8001a31e3e6e7afd44a9415553ca9bc468c1bbc7
Diego Neumann <diego.neumann.drese@gmail.com>
2022-06-16 20:35:24 -0300

    Bug fix method not found

--- src/Models/Permission.php
+++ src/Models/Permission.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Valiria\Models;
 
 use Illuminate\Database\Eloquent\Model;
@@ -7,6 +8,12 @@ use Valiria\Traits\HasRoles;
 class Permission extends Model
 {
     use HasRoles;
+
     protected $table = 'permission';
     protected $guarded = ['id'];
+
+    public static function getPermission($verb, $uri)
+    {
+        return self::where('verb', $verb)->where('uri', $uri)->first();
+    }
 }
