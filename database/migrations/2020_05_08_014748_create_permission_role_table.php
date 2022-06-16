6c054668905645c0f6831971f0be3007ca19bda0
Diego <diego.neumann.drese@gmail.com>
2022-06-16 17:33:32 -0300

    Update 2020_05_08_014748_create_permission_role_table.php
    Adjust name table

--- database/migrations/2020_05_08_014748_create_permission_role_table.php
+++ database/migrations/2020_05_08_014748_create_permission_role_table.php
@@ -13,9 +13,9 @@ return new class extends Migration {
     public function up() {
         Schema::create('permission_role', function (Blueprint $table) {
             $table->bigInteger('permission_id')->unsigned()->index();
-            $table->foreign('permission_id')->references('id')->on('permissions')->onDelete('cascade');
+            $table->foreign('permission_id')->references('id')->on('permission')->onDelete('cascade');
             $table->bigInteger('role_id')->unsigned()->index();
-            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+            $table->foreign('role_id')->references('id')->on('role')->onDelete('cascade');
             $table->primary(['permission_id', 'role_id']);
         });
     }
